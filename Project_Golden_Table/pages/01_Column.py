85df4ee676c6a45a2a3b901a003a34c358f42f85
CoderNidhiReddu <nidhireddu40@gmail.com>
2025-12-27 20:33:10 +0530

    Update 01_Column.py

--- Project_Golden_Table/pages/01_Column.py
+++ Project_Golden_Table/pages/01_Column.py
@@ -10,11 +10,11 @@ if "df" not in st.session_state:
     st.stop()
 df = st.session_state.df
 
-buffer = io.StringIO()
-df.info(buf=buffer)
-s = buffer.getvalue()
+# st.write("Show Columns with NaN values : ")
+# col_nan = df.columns[df.isna().any()].tolist()
+# t1,t2,t3,t4 = st.tabs(["Column Statistics","Duplicates","Missing Values"])
 
-st.summary(s)
+
 
 st.write("How you want to handle missing values ? ")
 c1,c2 = st.columns(2)
@@ -22,14 +22,28 @@ c1,c2 = st.columns(2)
 with c1:
     f1 = st.radio("Type :" , ["Fill with mean ","Fill with median ","Drop Rows", "Fill with"])
 
-fill_with = None
+fill_num = None
 if f1 == "Fill with":
-    num = st.number_input("",value=0)
+    fill_num = st.number_input("",value=0)
 
 
-st.button("Apply Changes ",type="primary")
+if st.button("Apply Changes ",type="primary"):
+    if f1 == "Fill with mean" :
+        numeric_mean = df.select_dtypes(include = ["number"]).mean()
+        df = df.fillna(numeric_mean)
+    elif f1 == "Fill with median" :
+        num_median = df.select_dtypes(include = ["number"]).median()
+        df.fillna(num_median, inplace = True)
+    elif f1 == "Drop Rows" :
+        df = df.dropna()
+    else:
+        df = df.fillna(fill_num)
 
 
-# handling missing values
+st.write("Data Preview ")
+st.dataframe(df)
+
+
 # show graphs between x and y
 # download file format
+        
