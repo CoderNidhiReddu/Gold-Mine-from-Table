caa7b7e41db60ce32f71211904ad3e6106fc9840
CoderNidhiReddu <nidhireddu40@gmail.com>
2025-12-30 12:15:00 +0530

    Updated

--- Project_Golden_Table/pages/01_Column.py
+++ Project_Golden_Table/pages/01_Column.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import io
 
-st.title("Data Analysis")
+st.markdown("<h1 style = 'color:#2196F0;'> Data Preprocessing</h1>",
+            unsafe_allow_html= True)
 
 st.set_page_config(page_title = "Handling missing values : ")
 
@@ -10,13 +11,36 @@ if "df" not in st.session_state:
     st.stop()
 df = st.session_state.df
 
-# st.write("Show Columns with NaN values : ")
-# col_nan = df.columns[df.isna().any()].tolist()
-# t1,t2,t3,t4 = st.tabs(["Column Statistics","Duplicates","Missing Values"])
+df_col = st.session_state.df.columns.tolist()
 
+c1,c2,c3 = st.columns(3)    
+c4,c5 = st.columns(2)
+with c1:
+    old_col = st.selectbox("Select Column", df_col, key="c1")
+with c2:
+    new_name = st.text_input("Enter the New Name : ")
+with c3 : 
+    st.write("")
+    st.write("")
+    if st.button("Rename ", type='primary', width= "stretch"):
+        if new_name:
+            st.session_state.df.rename(columns={old_col : new_name}, inplace = True)
 
+with c4:
+    col = st.selectbox("Select Column", df_col, key="c2")
+with c5:
+    st.write("")
+    st.write("")
+    if st.button("Drop Column", type="primary", width="stretch"):
+        st.session_state.df.drop(columns=[col], inplace= True)
 
-st.write("How you want to handle missing values ? ")
+# st.dataframe(st.session_state.df, width = "stretch")
+
+st.markdown(
+    "<h2 style='color:#2196F3;'>How you want to handle missing values ?</h2>",
+    unsafe_allow_html=True
+)
+
 c1,c2 = st.columns(2)
 
 with c1:
@@ -29,9 +53,11 @@ if f1 == "Fill with":
 
 if st.button("Apply Changes ",type="primary"):
     if f1 == "Fill with mean" :
+        # error
         numeric_mean = df.select_dtypes(include = ["number"]).mean()
         df = df.fillna(numeric_mean)
     elif f1 == "Fill with median" :
+        # error
         num_median = df.select_dtypes(include = ["number"]).median()
         df.fillna(num_median, inplace = True)
     elif f1 == "Drop Rows" :
@@ -40,10 +66,10 @@ if st.button("Apply Changes ",type="primary"):
         df = df.fillna(fill_num)
 
 
-st.write("Data Preview ")
+st.markdown("<h2 style = 'color:#2196F3;'> Data Preview </h2>",
+            unsafe_allow_html= True)
 st.dataframe(df)
 
 
 # show graphs between x and y
-# download file format
-        
+# column wise mean, mode, median, sum, avg, ...
