966bbffc63388d2bb38dd6f15f508ccc35607460
CoderNidhiReddu <nidhireddu40@gmail.com>
2025-12-30 12:14:32 +0530

    updated

--- Project_Golden_Table/Home.py
+++ Project_Golden_Table/Home.py
@@ -1,39 +1,25 @@
 import streamlit as st
 import pandas as pd
-st.title("Import Bronze Table ")
 
-file = st.file_uploader("Upload File : ", type=["csv"])
+st.title("Uploading Page")
 
-if file is None :
-    st.error("Upload file first !!")
+if "uploaded_file" not in st.session_state :
+    st.session_state.uploaded_file = None
+
+uploaded_file = st.file_uploader("Upload File : ", type = ["csv", "xlsx"])
+
+if uploaded_file is None:
+    st.error("Upload a file first !!")
+    st.stop()
 
 else:
+    # st.balloons()
     if "df" not in st.session_state:
-        st.session_state.df = pd.read_csv(file)
+        if uploaded_file.name.endswith(".csv"):
+            st.session_state.df = pd.read_csv(uploaded_file)
+        else:
+            st.session_state.df = pd.read_excel(uploaded_file)
 
-    df = st.session_state.df
-    df_col = df.columns.tolist()
+    
 
-    c1, c2 = st.columns(2)
-    c3, c4 = st.columns(2)
-    with c1:
-        old_col = st.selectbox("Select Column :",df_col)
-    with c2:
-        new_name = st.text_input("Enter Column new name : ")
-        if st.button("Rename Column"):
-            if new_name:
-                st.session_state.df = df.rename(columns = {old_col : new_name})
-                st.success("Column Name successfully Updated")
-
-    with c3:
-        col2 = st.selectbox("Select Column :",df_col, key = "col2")
-    with c4:
-        st.write("")
-        st.write("")
-        if st.button("Drop Column") :
-            st.session_state.df = df.drop(columns=[col2])
-        
-
-    st.write("Data Preview ")
-    st.dataframe(st.session_state.df)
-
+    st.dataframe(st.session_state.df, width = "stretch")
