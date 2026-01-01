df88818cf433ab4ba701b2e3862e2b5d916d4221
ExpensiveBug <nidhireddu40@gmail.com>
2026-01-01 08:59:46 +0530

    updated

--- Project_Golden_Table/Home.py
+++ Project_Golden_Table/Home.py
@@ -3,23 +3,23 @@ import pandas as pd
 
 st.title("Uploading Page")
 
+dummy_data = r"D:\Languages\Data\New_Pokemon_Data.csv"
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.read_csv(dummy_data)
+
 if "uploaded_file" not in st.session_state :
     st.session_state.uploaded_file = None
 
 uploaded_file = st.file_uploader("Upload File : ", type = ["csv", "xlsx"])
 
-if uploaded_file is None:
-    st.error("Upload a file first !!")
-    st.stop()
-
-else:
+if uploaded_file is not None:
     # st.balloons()
-    if "df" not in st.session_state:
-        if uploaded_file.name.endswith(".csv"):
-            st.session_state.df = pd.read_csv(uploaded_file)
-        else:
-            st.session_state.df = pd.read_excel(uploaded_file)
+    if uploaded_file.name.endswith(".csv"):
+        st.session_state.df = pd.read_csv(uploaded_file)
+    else:
+        st.session_state.df = pd.read_excel(uploaded_file)
 
-    
 
-    st.dataframe(st.session_state.df, width = "stretch")
+st.subheader("Dummy Data ")
+st.dataframe(st.session_state.df, width = "stretch")
