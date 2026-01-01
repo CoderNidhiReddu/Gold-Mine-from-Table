51d54bfe8bddb542beb3d83fe29854b750f6dfcf
ExpensiveBug <nidhireddu40@gmail.com>
2026-01-01 16:00:05 +0530

    done

--- Project_Golden_Table/pages/01_Preprocessing.py
+++ Project_Golden_Table/pages/01_Preprocessing.py
@@ -6,6 +6,7 @@ st.markdown("<h1 style = 'color:#2196F0;'> Data Preprocessing</h1>",
 
 st.set_page_config(page_title = "Handling missing values : ")
 
+# checking whether data is available in the session_state
 if "df" not in st.session_state:
     st.error("No Data available. ")
     st.stop()
@@ -13,9 +14,9 @@ if "df" not in st.session_state:
 df = st.session_state.df
 df_col = st.session_state.df.columns.tolist()
 
+# Rerun or update immediately only this part of df when changes applied 
 @st.fragment
 def column_op():
-
     c1,c2,c3 = st.columns(3)    
     c4,c5 = st.columns(2)
     with c1:
@@ -29,7 +30,6 @@ def column_op():
             if new_name:
                 st.session_state.df.rename(columns={old_col : new_name}, inplace = True)
                 st.rerun()
-
     with c4:
         col = st.selectbox("Select Column", df_col, key="c2")
     with c5:
@@ -39,44 +39,41 @@ def column_op():
             st.session_state.df.drop(columns=[col], inplace= True)
             st.rerun()
 
-column_op()
-# st.dataframe(st.session_state.df, width = "stretch")
+column_op()         #call the fragment
 
+# Handle Missing Data
 st.markdown(
     "<h2 style='color:#2196F3;'>How you want to handle missing values ?</h2>",
     unsafe_allow_html=True
 )
-
 c1,c2 = st.columns(2)
-
 with c1:
-    f1 = st.radio("Type :" , ["Fill with mean ","Fill with median ","Drop Rows", "Fill with"])
+    f1 = st.radio("Type :" , ["Fill with mean","Fill with median","Drop Rows", "Fill with"])
 
 fill_num = None
 if f1 == "Fill with":
     fill_num = st.number_input("",value=0)
-
-
+# filling values
 if st.button("Apply Changes ",type="primary"):
+    df = st.session_state.df
     if f1 == "Fill with mean" :
-        # error
-        numeric_mean = df.select_dtypes(include = ["number"]).mean()
+        numeric_mean = df.select_dtypes(include = "number").mean()
         df = df.fillna(numeric_mean)
+
     elif f1 == "Fill with median" :
-        # error
-        num_median = df.select_dtypes(include = ["number"]).median()
-        df.fillna(num_median, inplace = True)
+        num_median = df.select_dtypes(include = "number").median()
+        df = df.fillna(num_median)
+
     elif f1 == "Drop Rows" :
         df = df.dropna()
-    else:
-        df = df.fillna(fill_num)
+        
+    elif f1 == "Fill with":
+        numeric_cols = df.select_dtypes(include="number").columns
+        df[numeric_cols] = df[numeric_cols].fillna(fill_num)
 
+    st.session_state.df = df
+    st.rerun()
 
 st.markdown("<h2 style = 'color:#2196F3;'> Data Preview </h2>",
             unsafe_allow_html= True)
-st.dataframe(df)
-
-
-# show graphs between x and y
-# column wise mean, mode, median, sum, avg, ...
-
+st.dataframe(st.session_state.df)
