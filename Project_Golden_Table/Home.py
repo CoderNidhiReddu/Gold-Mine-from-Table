350fed9547fe4b73fff20afc467d4642e7950431
ExpensiveBug <nidhireddu40@gmail.com>
2026-01-01 16:01:48 +0530

    done

--- Project_Golden_Table/Home.py
+++ Project_Golden_Table/Home.py
@@ -1,19 +1,21 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.title("Uploading Page")
+st.markdown("<h1 style = 'color:#83f2d5;'>Uploading Page </h1>",
+            unsafe_allow_html=True)
 
 dummy_data = pd.DataFrame([
-    ["Ivysaur", "Grass", "Poison", 405, 60, 62, 63, 80, 80, 60, 1],
+    ["Ivysaur", "Grass", "Poison", 405, 60, np.nan, 63, 80, 80, 60, 1],
     ["Venusaur", "Grass", "Poison", 525, 80, 82, 83, 100, 100, 80, 1],
-    ["Venusaur Mega", "Grass", "Poison", 625, 80, 100, 123, 122, 120, 80, 1],
+    ["Venusaur Mega", "Grass", "Poison", 625, 80, 100, np.nan, 122, 120, 80, 1],
     ["Charmander", "Fire", None, 309, 39, 52, 43, 60, 50, 65, 1],
     ["Charmeleon", "Fire", None, 405, 58, 64, 58, 80, 65, 80, 1],
-    ["Charizard", "Fire", "Flying", 534, 78, 84, 78, 109, 85, 100, 1],
-    ["Charizard Mega X", "Fire", "Dragon", 634, 78, 130, 111, 130, 85, 100, 1],
+    ["Charizard", "Fire", "Flying", 534, 78, 84, np.nan, 109, 85, 100, 1],
+    ["Charizard Mega X", "Fire", "Dragon", 634, 78, 130, 111, 130, 85, np.nan, 1],
     ["Charizard Mega Y", "Fire", "Flying", 634, 78, 104, 78, 159, 115, 100, 1],
     ["Squirtle", "Water", None, 314, 44, 48, 65, 50, 64, 43, 1],
-    ["Blastoise Mega", "Water", None, 630, 79, 103, 120, 135, 115, 78, 1]],
+    ["Blastoise Mega", "Water", None, 630, 79, np.nan, 120, 135, 115, 78, 1]],
 columns = [
     "Name", "Type 1", "Type 2", "Total", "HP", "Attack",
     "Defense", "Sp. Atk", "Sp. Def", "Speed", "Generation"]
@@ -35,5 +37,6 @@ if uploaded_file is not None:
         st.session_state.df = pd.read_excel(uploaded_file)
 
 
-st.subheader("Dummy Data ")
+st.markdown("<h2 style = 'color:#49d3f5;'>Dummy Data </h2>",
+            unsafe_allow_html=True)
 st.dataframe(st.session_state.df, width = "stretch")
